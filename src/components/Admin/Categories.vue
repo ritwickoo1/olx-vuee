2e339fe2783d85bb18c88e7708860c914c615e27
Ritwick Verma <21mcmc09@uohyd.ac.in>
2023-01-30 16:34:15 +0530

    Updated PageNation

--- src/components/Admin/Categories.vue
+++ src/components/Admin/Categories.vue
@@ -1,97 +1,59 @@
 <template>
-    <center>
-    <div>
+<div>
     <h5 class=" btn-danger">Categories</h5>
-     <!-- add new Cat start -->
-     <input class="form-group bn" 
-     type="text"
-     placeholder="Category Name" 
-     >
-     <button
-      class="btn btn-success form-group bn">New Category</button>
-     <!-- add new cat end -->
-    <ul  class="list-group m">
-     <li class="z list-group-item d-flex justify-content-between align-items-center">
-       <input type="text" >
-        Cat Name 1
-       <div class="form-group ">
-           <button 
-           class="form-control btn btn-primary" > Edit</button>
-           <button 
-           class="form-control btn btn-success" > Save</button>
-           <!-- delete item -->
-           <button 
-           class="form-control btn btn-danger" > Delete</button>
-       </div>
-      </li>
-      <li class="z list-group-item d-flex justify-content-between align-items-center">
-       <input type="text" >
-        Cat Name 2
-       <div class="form-group ">
-           <button 
-           class="form-control btn btn-primary" > Edit</button>
-           <button 
-           class="form-control btn btn-success" > Save</button>
-           <!-- delete item -->
-           <button 
-           class="form-control btn btn-danger" > Delete</button>
-       </div>
-      </li>
-     </ul>
+    <!-- add new Cat start -->
+    <input class="form-group bn" type="text" placeholder="Category Name">
+    <button class="btn btn-success form-group bn">New Category</button>
+    <!-- add new cat end -->
+    <ul class="list-group m">
+        <li class="z list-group-item d-flex justify-content-between align-items-center">
+            <input type="text">
+            Cat Name 1
+            <div class="form-group ">
+                <button class="form-control btn btn-primary"> Edit</button>
+                <button class="form-control btn btn-success"> Save</button>
+                <!-- delete item -->
+                <button class="form-control btn btn-danger"> Delete</button>
+            </div>
+        </li>
+        <li class="z list-group-item d-flex justify-content-between align-items-center">
+            <input type="text">
+            Cat Name 2
+            <div class="form-group ">
+                <button class="form-control btn btn-primary"> Edit</button>
+                <button class="form-control btn btn-success"> Save</button>
+                <!-- delete item -->
+                <button class="form-control btn btn-danger"> Delete</button>
+            </div>
+        </li>
+    </ul>
+
+</div>
+</template>
+
    
-    </div>
-    </center>
-   
-   </template>
-   
-   
-   <style  scoped>
-   .userimg {
-           max-width: 10%;
-       border-radius: 50%;
-   }
-   .m {
-       margin: 10px auto;
-   }
-   
-   .wid{
-        margin-left: auto;
-       margin-right: auto;
-       width: 10em;
-   }
-   .z { 
-   font-size: 14px;
-   font-weight: bold;
-    }
-   
-   .bn{
-        margin: 5px ;
-   }
-   
-   
-   </style>
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
+<style scoped>
+.userimg {
+    max-width: 10%;
+    border-radius: 50%;
+}
+
+.m {
+    margin: 10px auto;
+}
+
+.wid {
+    margin-left: auto;
+    margin-right: auto;
+    width: 10em;
+}
+
+.z {
+    font-size: 14px;
+    font-weight: bold;
+}
+
+.bn {
+    margin: 5px;
+}
+</style>
