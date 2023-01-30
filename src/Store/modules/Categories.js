47aca0dde1994430607a4be975d1b4a976b750b1
Ritwick Verma <21mcmc09@uohyd.ac.in>
2023-01-30 16:48:11 +0530

    Pagenationstore updated

--- src/Store/modules/Categories.js
+++ src/Store/modules/Categories.js
@@ -1,49 +1,81 @@
 const state = {
     CateGorisList:[
-        // {id:'1', name:'phones'},
-        // {id:'2', name:'computers'},
-        // {id:'3', name:'shirts'},
-        // {id:'4', name:'shoes'},
-        // {id:'5', name:'cameras'},
-        // {id:'6', name:'cars'}
+        
     ],
     non:''
 }
 
-const getters={
-    allCategories:(state)=>(state.CateGorisList),
+const getters = {
+    allCategories:(state)=> state.CateGorisList,
 }
 
-const actions={
+const actions = {
     async GetCategories({commit}){
-        const newCatdata=[
+        const newCatdata = [
             {id:'11', name:'phones C'},
             {id:'22', name:'computers C'},
             {id:'33', name:'shirts C'},
             {id:'44', name:'shoes C'},
             {id:'55', name:'cameras C'},
-            {id:'66', name:'Bag C'}
+            {id:'66', name:'cars C'}
         ]
-        commit('setCategories',newCatdata)
+     commit('setCategories', newCatdata)
     },
-    async GetCatByID({commit},ID){
-        let item = state.CateGorisList.filter(x=> x.id == ID);
-        console.log('item id',ID,item);
-        commit('non');
-        return item;
+    async GetCatBYPageNumBer({commit}){
+        const newCatdata = [
+            {id:'77', name:'phones p'},
+            {id:'88', name:'computers p'},
+            {id:'99', name:'shirts p'},
+            {id:'100', name:'shoes p'},
+            {id:'101', name:'cameras p'},
+            {id:'102', name:'cars p'}
+        ]
+     commit('setCategories', newCatdata)
+    },
+    async ADD_NEW_CAT({commit}, data){
+        let NewDataArr = {};
+        let newID = Math.floor(Math.random() * 50) + 10;
+        NewDataArr = {id:newID, name:data.NewCatName};
+        commit('NewCategories', NewDataArr);
+    },
+    async EditOneCat({commit}, data){
+       let objIndex = state.CateGorisList.findIndex((obj => obj.id == data.ID));
+       let name = data.name;
+       let NewDatObj = {objIndex, name};
+       commit('EditOneCategories', NewDatObj)
+    },
+    async DeleteOneCat({commit}, data){
+     let newArrDel = state.CateGorisList.filter(x => {return x.id != data.ID});
+     commit('ReSetAndDelete', newArrDel);
+    },
+    async GetCatByID({commit}, ID){
+      let item = state.CateGorisList.filter(x => x.id == ID);
+      //console.log('item id', Id, item )
+      commit('non');
+      return item;
     }
-    
 }
-const mutations={
+
+
+const mutations = {
     setCategories:
-    (state, categories)=>(state.CateGorisList=categories),
+     (state, categories) => (state.CateGorisList = categories),
+     NewCategories:
+     (state, newData) => state.CateGorisList.unshift(newData),
+     EditOneCategories:(state, NewDatObj) =>
+      (state.CateGorisList[NewDatObj.objIndex].name = NewDatObj.name),
+      ReSetAndDelete:
+      (state, newArrDel) => (state.CateGorisList = newArrDel),
+      
     non:
-    (state)=> state.non=''
+     (state)=>(state.non = '')
 };
 
-export default{
+export default {
     state,
     getters,
     actions,
     mutations
 }
+
+
--- src/components/Admin/PageNationStore.vue
+++ src/components/Admin/PageNationStore.vue
@@ -1,54 +1,127 @@
 <template>
+<div class="container wid">
+    <div class="row">
+        <nav aria-label="Page navigation  ">
+            <ul class="pagination">
+                <li class="page-item">
+                    <button class="page-link" @click="NextPrevPage(false)">
+                        <span aria-hidden="true">&laquo;</span>
+                        <span class="sr-only">Previous</span>
+                    </button>
+                </li>
 
-  <div class="container wid">
-   <div class="row">
-    <nav aria-label="Page navigation  ">
-     <ul class="pagination">
-      <li class="page-item">
-        <button class="page-link"  >
-          <span aria-hidden="true">&laquo;</span>
-          <span class="sr-only">Previous</span>
-        </button>
-      </li>
-  
-    
-      <li class="page-item">
-        <button 
-        class="page-link active"  
-        >1</button>
-        <button 
-        class="page-link active"  
-        >2</button>
-      </li>
-    
-  
-      <li class="page-item">
-        <button class="page-link" >
-          <span aria-hidden="true">&raquo;</span>
-          <span class="sr-only" >Next</span>
-        </button>
-       </li>
-      </ul>
-     </nav>
+                <li v-for="(Page, idx) in PageArray" :key="idx" class="page-item">
+                    <button :class="PageSelected === Page ? 'page-link active':'page-link'" v-on:click="changePage(Page)">{{ Page }}</button>
+
+                </li>
+
+                <li class="page-item">
+                    <button @click="NextPrevPage(true)" class="page-link">
+                        <span aria-hidden="true">&raquo;</span>
+                        <span class="sr-only">Next</span>
+                    </button>
+                </li>
+            </ul>
+        </nav>
     </div>
-  </div>
+</div>
+</template>
+
   
+<script>
+import {
+    mapGetters,
+    mapActions
+} from 'vuex'
+
+export default {
+    name: 'PageNationStore',
+    data() {
+        return {
+            PageSelected: '',
+            PageArray: []
+        }
+    },
+    props: ['ComponentName'],
+    computed: {
+        ...mapGetters(['AllUsers', 'allCategories', 'AllProducs']),
+    },
+    mounted() {
+        if (!this.$route.query.page) {
+            this.$router.push({
+                path: '/',
+                name: 'Admin',
+                query: {
+                    page: 1,
+                }
+            }).catch(() => {})
+        }
+        this.PageSelected = parseInt(this.$route.query.page);
+        this.PageNation();
+
+    },
+    methods: {
+        ...mapActions([
+            'GetUsByPNum',
+            'GetCatBYPageNumBer',
+            'GetProdByPageNumber'
+        ]),
+        changePage(page) {
+            if (this.ComponentName == 'UsersComponent') {
+                this.GetUsByPNum(page);
+                let newd = this.AllUsers;
+                this.$emit('new-UserData', newd, page);
+            }
+            if (this.ComponentName == 'CategoryComponent') {
+                this.GetCatBYPageNumBer(page);
+                let newd = this.allCategories;
+                this.$emit('new-CategoryData', newd, page)
+            }
+            if (this.ComponentName == 'ProductComponent') {
+                this.GetProdByPageNumber(page);
+                let newd = this.AllProducs;
+                this.$emit('new-ProductsData', newd, page)
+            }
+            this.PageSelected = page;
+            this.$router.push({
+                path: '/',
+                name: 'Admin',
+                query: {
+                    page: page,
+                }
+            }).catch(() => {})
+        },
+        PageNation() {
+            this.PageArray = [];
+            let Scale = this.PageSelected + 3;
+            for (let index = this.PageSelected; index < Scale; index++) {
+                this.PageArray.push(index)
+            }
+        },
+        NextPrevPage(con) {
+            if (con == false && this.PageSelected == 1) {
+                console.log('non')
+            }
+            if (con == false && this.PageSelected !== 1) {
+                this.PageSelected = this.PageSelected - 1;
+                this.changePage(this.PageSelected);
+                this.PageNation();
+            }
+            if (con == true) {
+                this.PageSelected = this.PageSelected + 1;
+                this.changePage(this.PageSelected);
+                this.PageNation();
+            }
+        }
+    },
+}
+</script>
+
   
-  
-  </template>
-  
-  <style scoped>
-  .wid{
-       margin-left: auto;
-      margin-right: auto;
-      width: 10em;
-  }
-  </style>
-  
-  
-  
-  
-  
-  
-  
-  
+<style scoped>
+.wid {
+    margin-left: auto;
+    margin-right: auto;
+    width: 10em;
+}
+</style>
